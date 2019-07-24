fcbc2b0ce9ec7ba63b0224aea7a59ef32316130b
Radina <49740981+radinag@users.noreply.github.com>
2019-07-23 23:38:09 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Variables
-const cartBtnn = document.querySelector(".cart-btn");
+const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
 const cartDOM = document.querySelector(".cart");
@@ -8,7 +8,7 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".products-center");
-let cart = [];
+let cartt = [];
 
 // Getting products
 class Products {
